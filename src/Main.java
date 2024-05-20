7ffa54e4b50faa57646f3e48c862b12271b881bd
Akira1210 <jonathan.ponick2001@outlook.de>
2024-05-20 13:52:47 +0200

    Mehrfaches Vorkommen von Produkten behoben Problem behoben, dass identische Produkte in mehreren Auktionen zum Verkauf stehen konnten Die Anzahl der Auktionatoren wird nun auf die Anzahl der verschiedenen Produkten besschränkt

--- src/Main.java
+++ src/Main.java
@@ -29,6 +29,12 @@ public class Main {
         } catch (InputMismatchException eA) {
             HandleInputMismatchException(eA);
         }
+        //Sicherstellellen, dass genug Produkte für die Auktionatoren zur Verfügung stehen
+        if (numAuctioneers>Products.getItemAmount()) {
+            System.out.println("Es stehen nicht genug Produkte für die Auktionatoren zur Verfügung");
+            numAuctioneers=Products.getItemAmount();    //Falls nicht, wird die Anzahl der Auktionatoren auf die Anzahl der Produkte reduziert
+            System.out.println("Die Anzahl der Auktionaoren wurde auf die Anzahl der Verfügbaren Produkte beschränkt ("+numAuctioneers+" Auktionatoren).");
+        }
 
         System.out.println("\nBitte geben Sie die Anzahl der Bieter an:");
         try {
@@ -153,20 +159,26 @@ public class Main {
     // Auktionatoren wählen zufällig Produkte zum Verkaufen
     private static Products prodToAuctioneer() {
         Random rand = new Random();
-        boolean alreadyInAuction = false;
         Products prod = null;
-        int i = 0;
+        int i = -1;
+        int num =rand.nextInt(0, Products.getItemAmount() - 1); //Erstmal Zufallszahl, um zufälliges Produkte aus Liste zu erhalten
 
-        while (!alreadyInAuction) {
+        while (true) {
             i++;
-            prod = Products.getItem(rand.nextInt(0, Products.getItemAmount() - 1));
-            alreadyInAuction = prod.getInAuction();
-            if (i == Products.getItemAmount());
+            prod = Products.getItem(num);
+            if (!prod.getInAuction()) { //Überprüfen, dass Produkt nicht bereits in einer Auktion ist
+                Products.getItem(num).setInAuction(true);
+                return prod;
+            }
+            num = i; //Falls Zufallsprodukt bereits in einer Auktion ist, wird Produktliste von vrone bis hinten nach freien Produkten linear durchsucht
+            if (num == Products.getItemAmount()) //Falls alle Produkte in einer Auktion bereits zu finden sind (Eigentlich Unreachable Code, da nach Eingabe der Anzahl der AUktionaotren bereits sichergestellt wird, dass genug Produkte verfügbar sind)
             {
+                System.out.println("Auktionator konnte kein freies Produkt finden...");
                 break;
             }
+            
         }
-        return prod;
+        return null;
 
     }
 
